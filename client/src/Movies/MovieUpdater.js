99f0b7c5a76094a008a2627d7361407b752b6d5f
triggering-event <gabriel.j.shapiro@gmail.com>
2019-11-17 12:51:25 -0500

    adds a hook for managing entry of a new star and functionality for posting that star to the server through the API

--- client/src/Movies/MovieUpdater.js
+++ client/src/Movies/MovieUpdater.js
@@ -5,8 +5,13 @@ const MovieUpdater = (props) => {
 
     const [alteredMovie, setAlteredMovie] = useState(props.movie);
 
+    const [newMovieStar, setNewMovieStar] = useState("");
+
     const handleSubmit = () => {
         console.log("handle submit has been triggered");
+        let movieStars = alteredMovie.stars;
+        movieStars.push(newMovieStar);
+        setAlteredMovie({...alteredMovie, "stars": movieStars});
         /*do an axios post to change the entry in the server via the API */
         axios
             .put(`http://localhost:5000/api/movies/${props.movie.id}`, alteredMovie)
@@ -20,7 +25,11 @@ const MovieUpdater = (props) => {
 
     const handleChange = (e) => {
         console.log("handle change has been triggered");
-        setAlteredMovie({...alteredMovie, [e.target.name]: e.target.value});
+        if(e.target.name != "stars") {
+            setAlteredMovie({...alteredMovie, [e.target.name]: e.target.value});
+        } else {
+            setNewMovieStar(e.target.value);
+        }
     }
 
     return (
@@ -28,20 +37,30 @@ const MovieUpdater = (props) => {
             Title: <input type="text" name="title" value={alteredMovie.title} onChange={handleChange}/>
             Director: <input type="text" name="director" value={alteredMovie.director} onChange={handleChange}/>
             Metascore: <input type="text" name="metascore" value={alteredMovie.metascore} onChange={handleChange}/>
-            {alteredMovie.stars.map(star => {
-                return (
-                    <div>
-                        Star {alteredMovie.stars.indexOf(star)}:
-                        <input type="text"
-                               id={alteredMovie.stars.indexOf(star)}
-                               value={star}
-                               onChange={handleChange}/>
-                    </div>
-                )
-            })}
+            Add New Star: <input type="text" name="stars" value={newMovieStar} onChange={handleChange}/>
             <button>Submit Changes</button>
         </form>
     )
 }
 
 export default MovieUpdater;
+
+
+/*
+discarded code that maps through STARS array and allows editing each star
+
+discarded on basis that existing stars should not need to be edited, but kept in case functionality is needed later
+
+{alteredMovie.stars.map(star => {
+                return (
+                    <div>
+                        Edit Existing Star {alteredMovie.stars.indexOf(star)}:
+                        <input type="text"
+                               id={alteredMovie.stars.indexOf(star)}
+                               name="stars"
+                               value={star}
+                               onChange={handleChange}/>
+                    </div>
+                )
+            })}
+ */
